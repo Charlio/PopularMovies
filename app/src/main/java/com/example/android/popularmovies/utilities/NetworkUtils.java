b23211411f4ef39225b76fa8cddc985063df791a
Charlio Xu <chalioxumykj@gmail.com>
2018-01-14 15:53:47 -0800

    Add all java classes and methods  DetailActivity, MovieAdapter, utilities.NetworkUtils

---
+++ app/src/main/java/com/example/android/popularmovies/utilities/NetworkUtils.java
@@ -0,0 +1,20 @@
+package com.example.android.popularmovies.utilities;
+
+import android.support.annotation.Nullable;
+
+import java.io.IOException;
+import java.net.URL;
+
+/**
+ * Created by chali on 1/14/2018.
+ */
+
+public final class NetworkUtils {
+    @Nullable
+    public static URL buildUrl(String orderby) {
+        return null;
+    }
+    public static String getResponseFromHttpUrl(URL url) throws IOException {
+        return null;
+    }
+}
