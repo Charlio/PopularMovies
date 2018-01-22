1a2ec057288f23b07c210263962bc5635b4f8e2e
Charlio Xu <chalioxumykj@gmail.com>
2018-01-22 13:35:07 -0800

    implement data.MovieContract for movies table in the database

--- app/src/main/java/com/example/android/popularmovies/data/MovieContract.java
+++ app/src/main/java/com/example/android/popularmovies/data/MovieContract.java
@@ -1,8 +1,29 @@
 package com.example.android.popularmovies.data;
 
+import android.net.Uri;
+import android.provider.BaseColumns;
+
 /**
  * Created by chali on 1/21/2018.
  */
 
 public class MovieContract {
+
+    public static final String AUTHORITY = "com.example.android.popularmovies";
+    public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + AUTHORITY);
+    public static final String PATH_MOVIES = "movies";
+
+    public static final class MovieEntry implements BaseColumns {
+        public static final Uri CONTENT_URI =
+                BASE_CONTENT_URI.buildUpon().appendPath(PATH_MOVIES).build();
+
+        public static final String TABLE_NAME = "movies";
+
+        public static final String COLUMN_POSTER_RELATIVE_PATH = "poster_relative_path";
+        public static final String COLUMN_ORIGINAL_TITLE = "original title";
+        public static final String COLUMN_OVERVIEW = "overview";
+        public static final String COLUMN_VOTE_AVERAGE = "vote_average";
+        public static final String COLUMN_RELEASE_DATE = "release_date";
+    }
+
 }
