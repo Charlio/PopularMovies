76557d82d5c3073b3933b7614c4c5ae7d240d77b
Charlio Xu <chalioxumykj@gmail.com>
2018-01-22 13:48:36 -0800

    implement data.MovieDbHelper to create and update the movie table

--- app/src/main/java/com/example/android/popularmovies/data/MovieDbHelper.java
+++ app/src/main/java/com/example/android/popularmovies/data/MovieDbHelper.java
@@ -1,8 +1,39 @@
 package com.example.android.popularmovies.data;
 
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import com.example.android.popularmovies.data.MovieContract.MovieEntry;
+
 /**
  * Created by chali on 1/21/2018.
  */
 
-public class MovieDbHelper {
+public class MovieDbHelper extends SQLiteOpenHelper {
+
+    private static final String DATABASE_NAME = "moviesDb.db";
+    private static final int VERSION = 1;
+
+    MovieDbHelper(Context context) {
+        super(context, DATABASE_NAME, null, VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        final String CREATE_TABLE = "CREATE TABLE " + MovieEntry.TABLE_NAME + "(" +
+                MovieEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                MovieEntry.COLUMN_POSTER_RELATIVE_PATH + " TEXT, " +
+                MovieEntry.COLUMN_ORIGINAL_TITLE + " TEXT NOT NULL, " +
+                MovieEntry.COLUMN_OVERVIEW + " TEXT, " +
+                MovieEntry.COLUMN_VOTE_AVERAGE + " NUMBER NOT NULL, " +
+                MovieEntry.COLUMN_RELEASE_DATE + " TEXT NOT NULL);";
+        sqLiteDatabase.execSQL(CREATE_TABLE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + MovieEntry.TABLE_NAME);
+        onCreate(sqLiteDatabase);
+    }
 }
